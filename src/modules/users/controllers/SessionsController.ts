424bbfaf09fa84395501260ed08002feab530f14
João Vitor <67443955+jvlinkin@users.noreply.github.com>
2022-08-16 01:26:10 -0300

    Update SessionsController.ts
    Adicionei o await na momento de executar o serviço. Proposta: Corrigir o erro de "UnhandledPromiseRejectionWarning", que também estava acontecendo no meu projeto.

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -5,7 +5,7 @@ export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const createSession = new CreateSessionsService();
+    const createSession = await new CreateSessionsService();
 
     const user = createSession.execute({
       email,
